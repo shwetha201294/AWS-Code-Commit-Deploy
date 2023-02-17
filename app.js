b79255a8af3f1e86bd9c83e24a31b257b8783a66
shwetha201294 <121600493+shwetha201294@users.noreply.github.com>
2023-02-17 22:23:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.get('/products', (req, res) => {
   res.send([
     {
       productId: '101',
-      price: 100
+      price: 200
     },
     {
       productId: '102',
-      price: 200
+      price: 300
     }
   ])
 })
